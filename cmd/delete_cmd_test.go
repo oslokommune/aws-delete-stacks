52e64a2c476bff2aaf3088558b334b9bb1627c0e
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2021-03-27 10:02:06 +0100

    👌 Extract test helper

--- cmd/delete_cmd_test.go
+++ cmd/delete_cmd_test.go
@@ -1,12 +1,7 @@
 package cmd_test
 
 import (
-	"bytes"
 	"github.com/oslokommune/aws-delete-stacks/cloudformation_api"
-	"github.com/oslokommune/aws-delete-stacks/cmd"
-	"github.com/stretchr/testify/assert"
-	"io/ioutil"
-	"strings"
 	"testing"
 )
 
@@ -39,52 +34,4 @@ func TestDeleteCmd(t *testing.T) {
 	}
 
 	runTestCases(t, testCases)
-}
-
-func runTestCases(t *testing.T, testCases []*DeleteCmdTestCase) {
-	for _, tc := range testCases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			// Given
-			mock := NewCloudFormationMock()
-			mock.StackSummaries = tc.stackSummaries
-
-			// When
-			stdout, stderr := runDeleteCommand(t, mock, tc.args)
-
-			// Then
-			if tc.expectError {
-				for _, ec := range tc.expectedStderrContains {
-					assert.Contains(t, stderr, ec)
-				}
-			} else {
-				for _, ec := range tc.expectedStdoutContains {
-					assert.Contains(t, stdout, ec)
-				}
-			}
-		})
-	}
-}
-
-func runDeleteCommand(t *testing.T, cf cloudformation_api.CloudFormation, argsString string) (string, string) {
-	deleteAndArgs := strings.Trim(argsString, " ")
-	args := strings.Split(deleteAndArgs, " ")
-
-	var stdoutBuffer bytes.Buffer
-	var stderrBuffer bytes.Buffer
-
-	c := cmd.BuildDeleteCommand(cf)
-	c.SetArgs(args)
-	c.SetOut(&stdoutBuffer)
-	c.SetErr(&stderrBuffer)
-
-	err := c.Execute()
-
-	stdout, err := ioutil.ReadAll(&stdoutBuffer)
-	assert.NoError(t, err)
-
-	stderr, err := ioutil.ReadAll(&stderrBuffer)
-	assert.NoError(t, err)
-
-	return string(stdout), string(stderr)
 }
